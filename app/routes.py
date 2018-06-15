cbc94f6243485a4fff90cd8f572b1ae4b7a9d7f6
Ba Nyar Naing <banyarnaing123@gmail.com>
2018-06-15 21:51:01 +0630

    Add Ch2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Ba Nyar Naing'}
+    posts = [
+            {
+                'author': {'username': 'Ba Nyar'},
+                'body': 'Beautiful day in Myanmar'
+            },
+            {
+                'author': {'username': 'Shwe Wah'},
+                'body': 'The Catvengers movie was so cool!'
+            }
+        ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
